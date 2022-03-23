8a747d5bf3c272c7e835b8234dde21726c7961e4
mgonzalo99 <54797061+mgonzalo99@users.noreply.github.com>
2022-03-23 00:04:04 -0300

    test

--- utils/kebab-case.js
+++
@@ -1,9 +0,0 @@
-const getKebabCase = (text) =>
-    text
-        .match(/[a-zA-Z0-9]+/g)
-        ?.join("-")
-        .toLowerCase();
-
-const kebabCaseText = getKebabCase("Hey, how are you? I am fine");
-
-console.log(kebabCaseText);
--- utils/uniqueValues.js
+++
@@ -1,8 +0,0 @@
-const myArray = [1, 2, 3, 2, 10, 4, 10];
-
-// Get values that aren't repeated
-const getUniqueValues = (array) => [...new Set(array)];
-
-const uniqueValues = getUniqueValues(myArray);
-
-console.log(uniqueValues);
